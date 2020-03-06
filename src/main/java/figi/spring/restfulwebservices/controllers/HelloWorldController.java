41ec6b1ffb351ab5382de96608cdb1975a0ec8e7
adolfogarlez <adolfogarlez@gmail.com>
2020-03-06 18:39:56 +0800

    First commit

---
+++ src/main/java/figi/spring/restfulwebservices/controllers/HelloWorldController.java
@@ -0,0 +1,25 @@
+package figi.spring.restfulwebservices.controllers;
+
+import figi.spring.restfulwebservices.domain.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{message}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String message) {
+        return new HelloWorldBean(String.format("Hello World, %s", message));
+    }
+}
---
+++ src/main/java/figi/spring/restfulwebservices/domain/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package figi.spring.restfulwebservices.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class HelloWorldBean {
+
+    private String message;
+}
